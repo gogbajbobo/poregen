ad146b6344895aa2584f98f1175f148a563f3c7b
gogbajbobo <grigoriev.maxim@gmail.com>
2021-06-29 12:12:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,29 +42,40 @@ axes.imshow(img_show)
 # %%
 segments_lengths = helper.segments_lengths_from_image(img)
 ndim = img.ndim
-segments_pdfs = {}
+segments_kdes = {}
 
 fig, axes = plt.subplots(ndim * 2, 1, figsize=(10, ndim * 10))
 
 for d in np.arange(ndim):
-    segments_pdfs[d] = {}
+    segments_kdes[d] = {}
 
     pores_lengths = segments_lengths[d]['pores']
     p_hist, p_edges = helper.hist_of_lengths(pores_lengths)
-    _, p_pdf, p_linspace = helper.kde_of_lengths(pores_lengths)
-    segments_pdfs[d]['pores'] = p_pdf
+    p_kde, p_pdf, p_linspace = helper.kde_of_lengths(pores_lengths)
+    segments_kdes[d]['pores'] = p_kde
     
     axes[2 * d].bar(p_edges[:-1], p_hist, width=np.diff(p_edges), edgecolor="black", align="edge")
     axes[2 * d].plot(p_linspace, p_pdf, c='red')
 
     solid_lengths = segments_lengths[d]['solid']
     s_hist, s_edges = helper.hist_of_lengths(solid_lengths)
-    _, s_pdf, s_linspace = helper.kde_of_lengths(solid_lengths)
-    segments_pdfs[d]['solid'] = s_pdf
+    s_kde, s_pdf, s_linspace = helper.kde_of_lengths(solid_lengths)
+    segments_kdes[d]['solid'] = s_kde
     
     axes[2 * d + 1].bar(s_edges[:-1], s_hist, width=np.diff(s_edges), edgecolor="black", align="edge")
     axes[2 * d + 1].plot(s_linspace, s_pdf, c='red')
+
+# %%
+synt_img = np.array([])
+kdes = segments_kdes[0]
+
+while len(synt_img) < im_size:
+    segment = helper.get_sample(kdes['pores'])
+    synt_img = np.append(synt_img, np.zeros(segment))
+    segment = helper.get_sample(kdes['solid'])
+    synt_img = np.append(synt_img, np.ones(segment))
     
-print(segments_pdfs)
+plt.plot(synt_img)
+print(f'porosity: { 1 - np.sum(synt_img)/ synt_img.size}')
 
 # %%
