4d32d5beb2f996abdc2622e567de7495021d5919
gogbajbobo <grigoriev.maxim@gmail.com>
2021-06-29 11:18:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,43 +43,19 @@ segments_lengths = helper.get_segments_lengths_from_image(img)
 ndim = img.ndim
 
 fig, axes = plt.subplots(ndim * 2, 1, figsize=(10, ndim * 10))
+
 for d in np.arange(ndim):
-    pores_lengths = segments_lengths[d]['pores']
-    p_max_value = np.max(pores_lengths)
-    print(f'{ d } p_max_value { p_max_value }')
-    p_hist, p_edges = np.histogram(pores_lengths, bins=p_max_value)
-    p_hist_kde = sp.stats.gaussian_kde(p_hist)
-    p_hist = p_hist / np.sum(p_hist)
-    p_linspace = np.linspace(0, p_max_value, num=p_max_value + 1)
-    p_pdf = p_hist_kde.pdf(p_linspace)
     
+    pores_lengths = segments_lengths[d]['pores']
+    p_hist, p_edges = helper.hist_of_lengths(pores_lengths)
+    _, p_pdf, p_linspace = helper.kde_of_lengths(pores_lengths)
     axes[2 * d].bar(p_edges[:-1], p_hist, width=np.diff(p_edges), edgecolor="black", align="edge")
     axes[2 * d].plot(p_linspace, p_pdf, c='red')
 
     solid_lengths = segments_lengths[d]['solid']
-    s_max_value = np.max(solid_lengths)
-    print(f'{ d } s_max_value { s_max_value }')
-    s_hist, s_edges = np.histogram(solid_lengths, bins=s_max_value)
-    s_hist_kde = sp.stats.gaussian_kde(s_hist)
-    s_hist = s_hist / np.sum(s_hist)
-    s_linspace = np.linspace(0, s_max_value, num=s_max_value + 1)
-    s_pdf = s_hist_kde.pdf(s_linspace)
-
+    s_hist, s_edges = helper.hist_of_lengths(solid_lengths)
+    _, s_pdf, s_linspace = helper.kde_of_lengths(solid_lengths)
     axes[2 * d + 1].bar(s_edges[:-1], s_hist, width=np.diff(s_edges), edgecolor="black", align="edge")
-    axes[2 * d + 1].plot(s_pdf, c='red')
-
-# %%
-fig, axes = plt.subplots(ndim * 2, 1, figsize=(10, ndim * 10))
-for d in np.arange(ndim):
-    pores_lengths = segments_lengths[d]['pores']
-    solid_lengths = segments_lengths[d]['solid']
-    p_max_value = np.max(pores_lengths)
-    s_max_value = np.max(solid_lengths)
-    sns.histplot(pores_lengths, ax=axes[2 * d], bins=p_max_value, stat='probability', kde=True)
-    sns.histplot(solid_lengths, ax=axes[2 * d + 1], bins=s_max_value, stat='probability', kde=True)
-
-# %%
-
-# %%
+    axes[2 * d + 1].plot(s_linspace, s_pdf, c='red')
 
 # %%
