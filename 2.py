d4b87038a8c21afd2a92af274c3e5905ee3140f0
gogbajbobo <grigoriev.maxim@gmail.com>
2021-07-08 18:28:48 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -29,18 +29,20 @@ import seaborn as sns
 import helper
 
 # %%
-im_size = 1024*1024
-img = helper.one_D_generator(im_size, sigma=4, porosity=0.3, seed=0)
+im_size = 128*128
+porosity = 0.3
+img = helper.one_D_generator(im_size, sigma=4, porosity=porosity, seed=0)
 plt.figure(figsize=(20,5))
-plt.plot(img)
+plt.plot(img[:1024])
 print(f'porosity: { helper.image_porosity(img) }')
 
 # %%
 segments_lengths = helper.segments_lengths_from_image(img)[1]
-print(segments_lengths)
+# print(segments_lengths)
 
 segments_hists = {}
 segments_kdes = {}
+segments_pdfs = {}
 segments_cdfs = {}
 
 fig, axes = plt.subplots(2, 2, figsize=(20, 10))
@@ -50,6 +52,7 @@ p_hist, p_edges = helper.hist_of_lengths(pores_lengths)
 p_kde, p_pdf, p_cdf, p_linspace = helper.kde_of_lengths(pores_lengths)
 segments_hists['pores'] = p_hist
 segments_kdes['pores'] = p_kde
+segments_pdfs['pores'] = p_pdf
 segments_cdfs['pores'] = p_cdf
 
 axes[0, 0].bar(p_edges[:-1], p_hist, width=np.diff(p_edges), edgecolor="black", align="edge")
@@ -62,6 +65,7 @@ s_hist, s_edges = helper.hist_of_lengths(solid_lengths)
 s_kde, s_pdf, s_cdf, s_linspace = helper.kde_of_lengths(solid_lengths)
 segments_hists['solid'] = s_hist
 segments_kdes['solid'] = s_kde
+segments_pdfs['solid'] = s_pdf
 segments_cdfs['solid'] = s_cdf
 
 axes[1, 0].bar(s_edges[:-1], s_hist, width=np.diff(s_edges), edgecolor="black", align="edge")
@@ -87,13 +91,13 @@ while len(synt_img) < line_length:
 synt_img = synt_img[:line_length]
 
 plt.figure(figsize=(20, 5))
-plt.plot(synt_img)
+plt.plot(synt_img[:1024])
 
 print(f'porosity: { helper.image_porosity(synt_img) }')
 
 # %%
 t_segments_lengths = helper.segments_lengths_from_image(np.array([synt_img]))[0]
-print(t_segments_lengths)
+# print(t_segments_lengths)
 
 t_segments_hists = {}
 
@@ -156,5 +160,61 @@ s_hists_diff[:t_segments_hists['solid'].size] -= t_segments_hists['solid']
 
 print(np.sum(p_hists_diff))
 print(np.sum(s_hists_diff))
+
+# %%
+p_eq_hist = np.zeros(np.max([segments_hists['pores'].size, t_segments_hists['pores'].size]))
+t_p_eq_hist = np.zeros(np.max([segments_hists['pores'].size, t_segments_hists['pores'].size]))
+p_eq_hist[:segments_hists['pores'].size] = segments_hists['pores']
+t_p_eq_hist[:t_segments_hists['pores'].size] = t_segments_hists['pores']
+
+s_eq_hist = np.zeros(np.max([segments_hists['solid'].size, t_segments_hists['solid'].size]))
+t_s_eq_hist = np.zeros(np.max([segments_hists['solid'].size, t_segments_hists['solid'].size]))
+s_eq_hist[:segments_hists['solid'].size] = segments_hists['solid']
+t_s_eq_hist[:t_segments_hists['solid'].size] = t_segments_hists['solid']
+
+print(np.corrcoef(p_eq_hist, t_p_eq_hist))
+print(np.corrcoef(s_eq_hist, t_s_eq_hist))
+
+# %%
+f = open('/Users/grimax/Desktop/log.txt', 'w')
+f.close()
+
+test_img = np.zeros(im_size)
+last_seg = np.array([], dtype=np.int32)
+
+for i in np.arange(im_size):
+    if i == 0:
+        test_img[i] = sp.stats.bernoulli.rvs(porosity)
+        last_seg = np.append(last_seg, test_img[i])
+    else:
+        f = open('/Users/grimax/Desktop/log.txt', 'a')
+        f.write(f'i: {i}\n')
+        f.write(f'last_seg: {last_seg}\n')
+
+        kind = 'pores' if 0 in last_seg else 'solid'
+        p = 1 - segments_cdfs[kind](len(last_seg))
+#         result = sp.stats.bernoulli.rvs(1 - p if kind == 'pores' else p)
+        result = (1 - p if kind == 'pores' else p) > 0.5
+        test_img[i] = result
+        boundary = result != last_seg[0]
+        last_seg = np.array([result]) if boundary else np.append(last_seg, [result])
+
+        f.write(f'p: {p}\n')
+        f.write(f'result: {result}\n')
+        f.write(f'\n')
+
+        f.close()
+
+
+print(test_img)
+
+# %%
+plt.figure(figsize=(20, 5))
+plt.plot(test_img[:1024])
+
+print(f'porosity: { helper.image_porosity(test_img) }')
+
+# %%
+test_img[0:2]
 
 # %%
