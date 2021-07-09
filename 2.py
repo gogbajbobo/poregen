df1ace1e3ebb69dfc2ae37aaf6ade8232a90bd22
gogbajbobo <grigoriev.maxim@gmail.com>
2021-07-09 15:12:20 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -126,54 +126,8 @@ axes[1, 1].set_ylim([0, 1])
 # print(segments_hists)
 
 # %%
-print(sp.stats.mode(segments_lengths['pores']))
-print(sp.stats.mode(segments_lengths['solid']))
-print(sp.stats.mode(t_segments_lengths['pores']))
-print(sp.stats.mode(t_segments_lengths['solid']))
-
-# %%
-print(np.mean(segments_lengths['pores']))
-print(np.mean(segments_lengths['solid']))
-print(np.mean(t_segments_lengths['pores']))
-print(np.mean(t_segments_lengths['solid']))
-
-# %%
-print(np.std(segments_lengths['pores']))
-print(np.std(segments_lengths['solid']))
-print(np.std(t_segments_lengths['pores']))
-print(np.std(t_segments_lengths['solid']))
-
-# %%
-print(np.median(segments_lengths['pores']))
-print(np.median(segments_lengths['solid']))
-print(np.median(t_segments_lengths['pores']))
-print(np.median(t_segments_lengths['solid']))
-
-# %%
-p_hists_diff = np.zeros(np.max([segments_hists['pores'].size, t_segments_hists['pores'].size]))
-p_hists_diff[:segments_hists['pores'].size] = segments_hists['pores']
-p_hists_diff[:t_segments_hists['pores'].size] -= t_segments_hists['pores']
-
-s_hists_diff = np.zeros(np.max([segments_hists['solid'].size, t_segments_hists['solid'].size]))
-s_hists_diff[:segments_hists['solid'].size] = segments_hists['solid']
-s_hists_diff[:t_segments_hists['solid'].size] -= t_segments_hists['solid']
-
-print(np.sum(p_hists_diff))
-print(np.sum(s_hists_diff))
-
-# %%
-p_eq_hist = np.zeros(np.max([segments_hists['pores'].size, t_segments_hists['pores'].size]))
-t_p_eq_hist = np.zeros(np.max([segments_hists['pores'].size, t_segments_hists['pores'].size]))
-p_eq_hist[:segments_hists['pores'].size] = segments_hists['pores']
-t_p_eq_hist[:t_segments_hists['pores'].size] = t_segments_hists['pores']
-
-s_eq_hist = np.zeros(np.max([segments_hists['solid'].size, t_segments_hists['solid'].size]))
-t_s_eq_hist = np.zeros(np.max([segments_hists['solid'].size, t_segments_hists['solid'].size]))
-s_eq_hist[:segments_hists['solid'].size] = segments_hists['solid']
-t_s_eq_hist[:t_segments_hists['solid'].size] = t_segments_hists['solid']
-
-print(np.corrcoef(p_eq_hist, t_p_eq_hist))
-print(np.corrcoef(s_eq_hist, t_s_eq_hist))
+helper.compare_lengths(segments_lengths, t_segments_lengths)
+helper.compare_hists(segments_hists, t_segments_hists)
 
 # %%
 f = open('/Users/grimax/Desktop/log.txt', 'w')
@@ -193,8 +147,8 @@ for i in np.arange(im_size):
 
         kind = 'pores' if 0 in last_seg else 'solid'
         p = 1 - segments_cdfs[kind](len(last_seg))
-#         result = sp.stats.bernoulli.rvs(1 - p if kind == 'pores' else p)
-        result = (1 - p if kind == 'pores' else p) > 0.5
+        result = sp.stats.bernoulli.rvs(1 - p if kind == 'pores' else p)
+#         result = (1 - p if kind == 'pores' else p) > 0.5
         test_img[i] = result
         boundary = result != last_seg[0]
         last_seg = np.array([result]) if boundary else np.append(last_seg, [result])
@@ -215,6 +169,37 @@ plt.plot(test_img[:1024])
 print(f'porosity: { helper.image_porosity(test_img) }')
 
 # %%
-test_img[0:2]
+t2_segments_lengths = helper.segments_lengths_from_image(np.array([test_img]))[0]
+# print(t2_segments_lengths)
+
+t2_segments_hists = {}
+
+fig, axes = plt.subplots(2, 2, figsize=(20, 10))
+
+pores_lengths = t2_segments_lengths['pores']
+p_hist, p_edges = helper.hist_of_lengths(pores_lengths)
+p_kde, p_pdf, p_cdf, p_linspace = helper.kde_of_lengths(pores_lengths)
+t2_segments_hists['pores'] = p_hist
+
+axes[0, 0].bar(p_edges[:-1], p_hist, width=np.diff(p_edges), edgecolor="black", align="edge")
+axes[0, 0].plot(p_linspace, p_pdf, c='red')
+axes[0, 1].plot([p_cdf(x) for x in p_linspace])
+axes[0, 1].set_ylim([0, 1])
+
+solid_lengths = t2_segments_lengths['solid']
+s_hist, s_edges = helper.hist_of_lengths(solid_lengths)
+s_kde, s_pdf, s_cdf, s_linspace = helper.kde_of_lengths(solid_lengths)
+t2_segments_hists['solid'] = s_hist
+
+axes[1, 0].bar(s_edges[:-1], s_hist, width=np.diff(s_edges), edgecolor="black", align="edge")
+axes[1, 0].plot(s_linspace, s_pdf, c='red')
+axes[1, 1].plot([s_cdf(x) for x in s_linspace])
+axes[1, 1].set_ylim([0, 1])
+
+# print(segments_hists)
+
+# %%
+helper.compare_lengths(segments_lengths, t2_segments_lengths)
+helper.compare_hists(segments_hists, t2_segments_hists)
 
 # %%
