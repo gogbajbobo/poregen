ce5282cfd6688e0c9b7788f8fcc79a99ee7fef99
gogbajbobo <grigoriev.maxim@gmail.com>
2021-07-23 12:38:25 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,21 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# use p_remain / (p_remain + p_change) probability for generator in 2D case
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
