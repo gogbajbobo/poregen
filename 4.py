2625ba0deca40b51da9a76db384964d96f5ddf88
gogbajbobo <grigoriev.maxim@gmail.com>
2021-07-27 12:30:47 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -92,6 +92,89 @@ hist
 
 
 # %%
+ed0 = edge_distances[0].ravel()
+ed1 = edge_distances[1].ravel()
+mask_c = np.ma.mask_or(edge_distances[0].mask, edge_distances[1].mask)
+edc = np.column_stack((ed0.data, ed1.data)).reshape(32, 32, 2)
+edc = np.ma.array(edc, mask=mask_c)
+print(edc[0:1])
+
+# %%
+x = np.ma.array(
+    [
+        [0, 1, 2, 3], 
+        [0, 1, 0, 1], 
+        [0, 0, 1, 2], 
+        [0, 1, 2, 3],
+    ],
+    mask=[
+        [0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]
+)
+y = np.ma.array(
+    [
+        [0, 1, 0, 1], 
+        [0, 1, 2, 3], 
+        [0, 1, 2, 3],
+        [0, 0, 1, 2], 
+    ],
+    mask=[
+        [1, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+    ]
+)
+
+x, np.rot90(y, k=-1)
+
+# %%
+xy = np.ma.array(
+    [
+        [[0, 0], [1, 0], [2, 0], [3, 0]], 
+        [[0, 0], [1, 1], [0, 1], [1, 1]], 
+        [[0, 1], [0, 2], [1, 2], [2, 0]], 
+        [[0, 2], [1, 3], [2, 3], [3, 1]],
+    ],
+    mask=[
+        [[0, 1], [0, 0], [0, 0], [0, 1]],
+        [[1, 0], [1, 0], [0, 0], [0, 1]],
+        [[1, 0], [0, 0], [0, 0], [0, 0]],
+        [[0, 0], [0, 0], [0, 0], [0, 0]],
+    ]
+)
+
+for ij in np.ndindex(xy.shape[:2]):
+    val = xy.mask[ij]
+    xy.mask[ij] = [1, 1] if np.any(xy.mask[ij]) else val
+
+# xy
+
+# %%
+y_rot = np.rot90(y, k=-1)
+
+x_data = x.data.ravel()
+x_mask = x.mask.ravel()
+
+y_data = y_rot.data.ravel()
+y_mask = y_rot.mask.ravel()
+
+x_y = np.column_stack((x_data, y_data)).reshape(4, 4, 2)
+x_y_mask = np.column_stack((x_mask, y_mask)).reshape(4, 4, 2)
+
+for ij in np.ndindex(x_y_mask.shape[:2]):
+    val = x_y_mask[ij]
+    x_y_mask[ij] = [1, 1] if np.any(x_y_mask[ij]) else val
+
+x_y = np.ma.array(x_y, mask=x_y_mask)
+
+print(f'data: {(x_y.data == xy.data).all()}')
+print(f'mask: {(x_y.mask == xy.mask).all()}')
+
+# %%
 segments_lengths = helper.segments_lengths_from_image(img)
 ndim = img.ndim
 
