b66a3946abe48aecc3d51f7f7252583687dbdc4f
gogbajbobo <grigoriev.maxim@gmail.com>
2021-08-06 15:52:13 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -30,7 +30,7 @@ import matplotlib.pyplot as plt
 import helper
 
 # %%
-im_size = 4
+im_size = 8
 dim = 2
 porosity = 0.5
 im_shape = np.ones(dim, dtype=np.int32) * im_size
@@ -48,10 +48,10 @@ image_statistics = helper.image_statistics(img)
 edge_distances = image_statistics['edge_distances']
 segments_lengths = image_statistics['segments_lengths']
 # print(np.int32(img_show))
-print(np.int32(edge_distances[0].data + 1))
-print(np.int32(edge_distances[1].data + 1))
-print(segments_lengths[0])
-print(segments_lengths[1])
+# print(np.int32(edge_distances[0].data + 1))
+# print(np.int32(edge_distances[1].data + 1))
+# print(segments_lengths[0])
+# print(segments_lengths[1])
 
 # %%
 x = edge_distances[0]
@@ -61,19 +61,21 @@ x_data = x.data.ravel()
 y_data = y.T.data.ravel()
 
 eds_shape = (*img.shape, dim)
-eds = np.column_stack((x_data, y_data)).reshape(eds_shape)
+eds = np.column_stack((y_data, x_data)).reshape(eds_shape)
 
 print(np.int32(img))
 # print(eds[:, :, 0])
 # print(eds[:, :, 1])
 
 # %%
-els_x = np.pad(eds[:, :, 0], 1)
+els_x = np.pad(edge_distances[0], 1)
 els_x[0, :] = -1
 els_x[:, 0] = -1
 mask_x = els_x != 0
 els_x[-1, :] = -1
-els_x = np.ma.array(np.roll(els_x, 1) + 1, mask=mask_x)
+# els_x = np.ma.array(np.roll(els_x, 1) + 1, mask=mask_x)
+els_x = np.roll(els_x, 1) + 1
+els_x[mask_x] = 0
 els_x = els_x[1:, 1:]
 els_x
 
@@ -83,13 +85,27 @@ els_y[0, :] = -1
 els_y[:, 0] = -1
 mask_y = els_y != 0
 els_y[-1, :] = -1
-els_y = np.ma.array(np.roll(els_y, 1) + 1, mask=mask_y)
+# els_y = np.ma.array(np.roll(els_y, 1) + 1, mask=mask_y)
+els_y = np.roll(els_y, 1) + 1
+els_y[mask_y] = 0
 els_y = els_y.T[1:, 1:]
 els_y
 
 # %%
 els_shape = (*tuple(i + 1 for i in img.shape), dim)
-els = np.column_stack((els_x.data.ravel(), els_y.data.T.ravel())).reshape(els_shape)
-els
+els = np.column_stack((els_y.ravel(), els_x.ravel())).reshape(els_shape)
+# els
+
+# %%
+fig, axes = plt.subplots(1, 1, figsize=(15, 15))
+im = np.pad(np.float32(img_show), 1, constant_values=.5)[1:, 1:]
+axes.imshow(im)
+
+for i in range(im.shape[0]):
+    for j in range(im.shape[1]):
+        color = 'black' if im[i, j] else 'w'
+        if i < eds.shape[0] and j < eds.shape[1]:
+            axes.text(j, i, eds[i, j], ha='right', va='bottom', c=color, size='x-large')
+        axes.text(j, i, els[i, j], ha='left', va='top', c=color, size='xx-large')
 
 # %%
