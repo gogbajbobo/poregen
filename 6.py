9a6fa8227b34bd5649663a58e94398479966788a
gogbajbobo <grigoriev.maxim@gmail.com>
2021-08-08 18:48:20 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -27,32 +27,38 @@ import pandas as pd
 
 from matplotlib import cm
 import matplotlib.pyplot as plt
+from matplotlib import colors
 
 import helper
 
 # %%
-im_size = 8
+import seaborn as sns
+sns.set_theme()
+sns.set_style("whitegrid")
+
+# %% tags=[]
+im_size = 256
 dim = 2
 porosity = 0.5
 im_shape = np.ones(dim, dtype=np.int32) * im_size
 np.random.seed(0)
 img = ~ps.generators.blobs(im_shape, porosity=porosity, blobiness=1)
 
-# %%
+# %% tags=[]
 fig, axes = plt.subplots(1, 1, figsize=(5, 5))
 img_show = img if dim == 2 else img[0, :, :] if dim == 3 else []
 axes.imshow(img_show)
 print(f'porosity: { helper.image_porosity(img) }')
 
-# %%
+# %% tags=[]
 image_statistics = helper.image_statistics(img)
 edge_distances = image_statistics['edge_distances']
 eds_y = (np.int32(edge_distances[1]) + 1).T
 eds_x = np.int32(edge_distances[0]) + 1
-print(eds_y)
-print(eds_x)
+# print(eds_y)
+# print(eds_x)
 
-# %%
+# %% tags=[]
 y_size = img.shape[-2]
 x_size = img.shape[-1]
 y_grid = np.arange(y_size)
@@ -92,6 +98,35 @@ for y in y_grid:
             'topIsSolid': rigth_is_solid,
         })
 
-df[df.notna().all(axis='columns')].shape
+dff = df[df.notna().all(axis='columns')]
+print(df.shape)
+print(dff.shape)
+
+# %% tags=[]
+dff.head()
+
+# %% tags=[]
+fig, axes = plt.subplots(1, 1, figsize=(10, 10))
+sns.histplot(dff, x='leftLength', y='topLength', bins=(dff['leftLength'].max(), dff['topLength'].max()))
+
+# %% tags=[]
+dff1 = dff[(dff['isSolid'] == dff['leftIsSolid']) & (dff['isSolid'] == dff['topIsSolid'])]
+print(dff1.shape)
+dff1.head()
+
+# %% tags=[]
+dff2 = dff[dff['leftIsSolid'] != dff['topIsSolid']]
+print(dff2.shape)
+dff2.head()
+
+# %% tags=[]
+fig, axes = plt.subplots(1, 2, figsize=(20, 10))
+sns.histplot(dff1, x='leftLength', y='topLength', bins=(dff1['leftLength'].max(), dff1['topLength'].max()), ax=axes[0])
+sns.histplot(dff2, x='leftLength', y='topLength', bins=(dff2['leftLength'].max(), dff2['topLength'].max()), ax=axes[1])
+
+# %% tags=[]
+g = sns.PairGrid(dff, vars=['leftLength', 'topLength'])
+g.map_diag(sns.histplot)
+g.map_offdiag(sns.scatterplot)
 
 # %%
