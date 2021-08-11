86c11402540a2d5fe56f52f32f0810c01e91dbc0
gogbajbobo <grigoriev.maxim@gmail.com>
2021-08-11 13:15:37 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -30,6 +30,12 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
+import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+
 import helper
 
 # %% tags=[]
@@ -44,108 +50,22 @@ dim = 2
 porosity = 0.3
 blobiness = 1
 im_shape = np.ones(dim, dtype=np.int32) * im_size
+
 np.random.seed(0)
 img = ~ps.generators.blobs(im_shape, porosity=porosity, blobiness=blobiness)
 
-# %% tags=[]
 fig, axes = plt.subplots(1, 1, figsize=(5, 5))
 img_show = img if dim == 2 else img[0, :, :] if dim == 3 else []
 axes.imshow(img_show)
 print(f'porosity: { helper.image_porosity(img) }')
 
 # %% tags=[]
-image_statistics = helper.image_statistics(img)
-edge_distances = image_statistics['edge_distances']
-eds_y = (np.int32(edge_distances[1]) + 1).T
-eds_x = np.int32(edge_distances[0]) + 1
-# print(eds_y)
-# print(eds_x)
+df = helper.dataframe_from_image(img)
+df.head()
 
 # %% tags=[]
-y_size = img.shape[-2]
-x_size = img.shape[-1]
-y_grid = np.arange(y_size)
-x_grid = np.arange(x_size)
-indices = pd.MultiIndex.from_tuples(list(np.ndindex(y_size, x_size)))
-
-df = pd.DataFrame(columns=['isSolid', 'leftLength', 'leftIsSolid', 'topLength', 'topIsSolid'], index=indices)
-
-for y in y_grid:
-    for x in x_grid:
-        
-        is_solid = bool(img[y, x])
-        left_length = np.NaN
-        left_is_solid = np.NaN
-        rigth_length = np.NaN
-        rigth_is_solid = np.NaN
-        
-        if x > 0:
-            prev_x = x - 1
-            is_masked = eds_x.mask[y, prev_x]
-            if not is_masked:
-                left_length = eds_x.data[y, prev_x]
-                left_is_solid = bool(img[y, prev_x])
-
-        if y > 0:
-            prev_y = y - 1
-            is_masked = eds_y.mask[prev_y, x]
-            if not is_masked:
-                rigth_length = eds_y.data[prev_y, x]
-                rigth_is_solid = bool(img[prev_y, x])
-
-        df.loc[(y, x)] = pd.Series({
-            'isSolid': is_solid, 
-            'leftLength': left_length, 
-            'leftIsSolid': left_is_solid, 
-            'topLength': rigth_length, 
-            'topIsSolid': rigth_is_solid,
-        })
-
-print(df.info())
-print(df.shape)
-        
-dff = df[df.notna().all(axis='columns')].astype(np.int32)
-
-print(dff.info())
-print(dff.shape)
-
-# %% tags=[]
-# dff.head()
-
-# %% tags=[]
-# fig, axes = plt.subplots(1, 1, figsize=(10, 10))
-# sns.histplot(dff, x='leftLength', y='topLength', bins=(dff['leftLength'].max(), dff['topLength'].max()))
-
-# %% tags=[]
-# dff1 = dff[(dff['isSolid'] == dff['leftIsSolid']) & (dff['isSolid'] == dff['topIsSolid'])]
-# print(dff1.shape)
-# dff1.head()
-
-# %% tags=[]
-# dff2 = dff[dff['leftIsSolid'] != dff['topIsSolid']]
-# print(dff2.shape)
-# dff2.head()
-
-# %% tags=[]
-# fig, axes = plt.subplots(1, 2, figsize=(20, 10))
-# sns.histplot(dff1, x='leftLength', y='topLength', bins=(dff1['leftLength'].max(), dff1['topLength'].max()), ax=axes[0])
-# sns.histplot(dff2, x='leftLength', y='topLength', bins=(dff2['leftLength'].max(), dff2['topLength'].max()), ax=axes[1])
-
-# %% tags=[]
-# g = sns.PairGrid(dff, vars=['leftLength', 'topLength'])
-# g.map_diag(sns.histplot)
-# g.map_offdiag(sns.scatterplot)
-
-# %% tags=[]
-import statsmodels.api as sm
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import plot_confusion_matrix
-from sklearn.model_selection import train_test_split
-
-# %% tags=[]
-X = dff[['leftLength', 'leftIsSolid', 'topLength', 'topIsSolid']]
-Y = dff[['isSolid']]
+X = df[['leftLength', 'leftIsSolid', 'topLength', 'topIsSolid']]
+Y = df[['isSolid']]
 x_train, x_test, y_train, y_test = train_test_split(X, Y)
 log_reg = sm.Logit(y_train, x_train).fit()
 print(log_reg.summary())
@@ -165,6 +85,41 @@ print(skl_log_reg.coef_)
 print(f'train: {skl_log_reg.score(X_train, Y_train)}')
 print(f'test: {skl_log_reg.score(X_test, Y_test)}')
 plot_confusion_matrix(skl_log_reg, X_test, Y_test)
+
+# %% tags=[]
+y = [y for y, x in Y[Y.isSolid != skl_log_reg.predict(X)].index]
+x = [x for y, x in Y[Y.isSolid != skl_log_reg.predict(X)].index]
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+img_show = img if dim == 2 else img[0, :, :] if dim == 3 else []
+axes[0].imshow(img_show)
+axes[1].imshow(img_show)
+axes[1].scatter(x, y, color='red', marker='.')
+
+# %% tags=[]
+np.random.seed(1)
+img_test = ~ps.generators.blobs(im_shape, porosity=porosity, blobiness=blobiness)
+fig, axes = plt.subplots(1, 1, figsize=(5, 5))
+img_test_show = img_test if dim == 2 else img_test[0, :, :] if dim == 3 else []
+axes.imshow(img_test_show)
+print(f'porosity: { helper.image_porosity(img_test) }')
+
+# %% tags=[]
+df_test = helper.dataframe_from_image(img_test)
+df_test.head()
+
+# %% tags=[]
+XX = df_test[['leftLength', 'leftIsSolid', 'topLength', 'topIsSolid']]
+YY = df_test[['isSolid']]
+
+y = [y for y, x in YY[YY.isSolid != skl_log_reg.predict(XX)].index]
+x = [x for y, x in YY[YY.isSolid != skl_log_reg.predict(XX)].index]
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 5))
+img_test_show = img_test if dim == 2 else img_test[0, :, :] if dim == 3 else []
+axes[0].imshow(img_test_show)
+axes[1].imshow(img_test_show)
+axes[1].scatter(x, y, color='red', marker='.')
 
 # %% tags=[]
 parameters = {'C': [.0001, .001, .01, .1, 1, 10, 100], 'solver': ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']}
