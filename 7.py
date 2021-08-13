a64404ad8031e5f7c919d807748379e07c4e68d5
gogbajbobo <grigoriev.maxim@gmail.com>
2021-08-13 14:53:02 +0300

    Update 7.py

--- 7.py
+++ 7.py
@@ -20,7 +20,7 @@
 # %load_ext autoreload
 # %autoreload 2
 
-# %%
+# %% tags=[]
 import porespy as ps
 import numpy as np
 import pandas as pd
@@ -30,13 +30,19 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
+import statsmodels.api as sm
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import plot_confusion_matrix
+from sklearn.model_selection import train_test_split
+
 import helper
 
-# %%
-im_size = 64
+# %% tags=[]
+im_size = 128
 dim = 2
 porosity = 0.5
-blobiness = 0.5
+blobiness = 2
 im_shape = np.ones(dim, dtype=np.int32) * im_size
 
 np.random.seed(0)
@@ -48,12 +54,87 @@ img_show = img if dim == 2 else img[0, :, :] if dim == 3 else []
 axes.imshow(img_show)
 print(f'porosity: { helper.image_porosity(img) }')
 
-# %%
+# %% tags=[]
 x_distances_solid, x_distances_void, y_distances_solid, y_distances_void = helper.border_distances_for_image(img)
 fig, axes = plt.subplots(1, 4, figsize=(20, 5))
 axes[0].imshow(x_distances_solid)
 axes[1].imshow(x_distances_void)
 axes[2].imshow(y_distances_solid)
 axes[3].imshow(y_distances_void)
+
+# %% tags=[]
+df = helper.dataframe_with_distances_from_image(img, (x_distances_solid, x_distances_void, y_distances_solid, y_distances_void))
+df
+
+# %% tags=[]
+df.corr().style.background_gradient(axis=None)
+
+# %% tags=[]
+X = df[['xDistanceSolid', 'xDistanceVoid', 'yDistanceSolid', 'yDistanceVoid']]
+Y = df[['isSolid']]
+x_train, x_test, y_train, y_test = train_test_split(X, Y)
+log_reg = sm.Logit(y_train, x_train).fit()
+print(log_reg.summary())
+predicted_train = log_reg.predict(x_train) > .5
+predicted_test = log_reg.predict(x_test) > .5
+print(f'train score: {(predicted_train.to_numpy().ravel() == y_train.to_numpy().ravel()).mean()}')
+print(f'test score: {(predicted_test.to_numpy().ravel() == y_test.to_numpy().ravel()).mean()}')
+
+# %% tags=[]
+skl_log_reg = LogisticRegression(fit_intercept = False, penalty='none')
+X_train = x_train
+Y_train = y_train.to_numpy().ravel()
+X_test = x_test
+Y_test = y_test.to_numpy().ravel()
+skl_log_reg.fit(X_train, Y_train)
+print(skl_log_reg.coef_)
+print(f'train: {skl_log_reg.score(X_train, Y_train)}')
+print(f'test: {skl_log_reg.score(X_test, Y_test)}')
+plot_confusion_matrix(skl_log_reg, X_test, Y_test)
+
+# %% tags=[]
+predict_image = skl_log_reg.predict(X)
+y = [y for y, x in Y[Y.isSolid != predict_image].index]
+x = [x for y, x in Y[Y.isSolid != predict_image].index]
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+img_show = img if dim == 2 else img[0, :, :] if dim == 3 else []
+axes[0].imshow(img_show)
+axes[1].imshow(predict_image.reshape(img.shape))
+axes[2].imshow(img_show)
+axes[2].scatter(x, y, color='red', marker='.')
+
+# %% tags=[]
+np.random.seed(1)
+img_test = ~ps.generators.blobs(im_shape, porosity=porosity, blobiness=blobiness)
+fig, axes = plt.subplots(1, 1, figsize=(5, 5))
+img_test_show = img_test if dim == 2 else img_test[0, :, :] if dim == 3 else []
+axes.imshow(img_test_show)
+print(f'porosity: { helper.image_porosity(img_test) }')
+
+# %% tags=[]
+df_test = helper.dataframe_with_distances_from_image(img_test)
+df_test
+
+# %% tags=[]
+XX = df_test[['xDistanceSolid', 'xDistanceVoid', 'yDistanceSolid', 'yDistanceVoid']]
+YY = df_test[['isSolid']]
+
+predict_image = skl_log_reg.predict(XX)
+incorrect_idxs = YY[YY.isSolid != predict_image].index
+
+y = [y for y, x in incorrect_idxs]
+x = [x for y, x in incorrect_idxs]
+
+fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+img_test_show = img_test if dim == 2 else img_test[0, :, :] if dim == 3 else []
+axes[0].imshow(img_test_show)
+axes[1].imshow(predict_image.reshape(img_test.shape))
+axes[2].imshow(img_test_show)
+axes[2].scatter(x, y, color='red', marker='.')
+
+# %% tags=[]
+print(f'score: {skl_log_reg.score(XX, YY)}')
+plot_confusion_matrix(skl_log_reg, XX, YY)
 
 # %%
