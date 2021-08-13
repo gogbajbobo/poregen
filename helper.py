ecc4a80bcde2822745590cccebec7d24be6f4d10
gogbajbobo <grigoriev.maxim@gmail.com>
2021-08-13 13:18:43 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -414,3 +414,61 @@ def pattern_dataframe_from_image(img):
     
     return df
 
+
+def borders_from_image(img):
+    x_borders = None
+    x_borders_solid = None
+    x_borders_void = None
+    y_borders = None
+    y_borders_solid = None
+    y_borders_void = None
+
+    for y in np.arange(img.shape[-2]):
+        row = img[y, :]
+        borders = row[1:] != row[:-1]
+        borders = np.append(True, borders)
+        indices = np.where(borders)[0]
+        points = [(y, idx) for idx in indices]
+        x_borders = points if x_borders is None else np.append(x_borders, points, axis=0)
+        x_borders_solid = np.array([(y, x) for y, x in x_borders if img[y, x] == 1])
+        x_borders_void = np.array([(y, x) for y, x in x_borders if img[y, x] == 0])
+
+    for x in np.arange(img.shape[-1]):
+        row = img[:, x]
+        borders = row[1:] != row[:-1]
+        borders = np.append(True, borders)
+        indices = np.where(borders)[0]
+        points = [(idy, x) for idy in indices]
+        y_borders = points if y_borders is None else np.append(y_borders, points, axis=0)
+        y_borders_solid = np.array([(y, x) for y, x in y_borders if img[y, x] == 1])
+        y_borders_void = np.array([(y, x) for y, x in y_borders if img[y, x] == 0])
+        
+    return x_borders_solid, x_borders_void, y_borders_solid, y_borders_void
+
+
+def border_distances_for_image(img):
+    
+    x_distances_solid = np.empty(img.shape, dtype=np.int32)
+    x_distances_void = np.empty(img.shape, dtype=np.int32)
+    y_distances_solid = np.empty(img.shape, dtype=np.int32)
+    y_distances_void = np.empty(img.shape, dtype=np.int32)
+
+    x_borders_solid, x_borders_void, y_borders_solid, y_borders_void = borders_from_image(img)
+    
+    def get_distance(point, borders, direction='x'):
+        fixed = -2 if direction == 'x' else -1
+        searched = -1 if direction == 'x' else -2
+        distances = [
+            point[searched] - b[searched] for b in borders if b[fixed] == point[fixed] and b[searched] <= point[searched]
+        ]
+        return np.min(distances) if len(distances) else -1
+
+    for y in np.arange(img.shape[-2]):
+        for x in np.arange(img.shape[-1]):
+            p = (y, x)
+            x_distances_solid[y, x] = get_distance(p, x_borders_solid)
+            x_distances_void[y, x] = get_distance(p, x_borders_void)
+            y_distances_solid[y, x] = get_distance(p, y_borders_solid, direction='y')
+            y_distances_void[y, x] = get_distance(p, y_borders_void, direction='y')
+
+    return x_distances_solid, x_distances_void, y_distances_solid, y_distances_void
